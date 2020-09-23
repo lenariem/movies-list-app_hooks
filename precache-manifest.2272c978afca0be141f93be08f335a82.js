0ad9dc92d375c8d1c1fa26dc284a2929dadd98b8
LenaIT <olena.denysova@gmail.com>
2020-09-23 21:05:07 +0200

    Updates

---
+++ precache-manifest.2272c978afca0be141f93be08f335a82.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef1d9e2b1e821893de1163d69f35c936",
+    "url": "/lenariem/movies-list-app_hooks/index.html"
+  },
+  {
+    "revision": "e9a097c8938932a32ccf",
+    "url": "/lenariem/movies-list-app_hooks/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "24ce3adf3b5558805a7b",
+    "url": "/lenariem/movies-list-app_hooks/static/css/main.49e34e9a.chunk.css"
+  },
+  {
+    "revision": "e9a097c8938932a32ccf",
+    "url": "/lenariem/movies-list-app_hooks/static/js/2.4a08473d.chunk.js"
+  },
+  {
+    "revision": "8a0f87d4ff9462a28d37926fe68f0ebe",
+    "url": "/lenariem/movies-list-app_hooks/static/js/2.4a08473d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ce3adf3b5558805a7b",
+    "url": "/lenariem/movies-list-app_hooks/static/js/main.f28f3709.chunk.js"
+  },
+  {
+    "revision": "81c0fcccf9f8e7641d9c",
+    "url": "/lenariem/movies-list-app_hooks/static/js/runtime-main.87540c4f.js"
+  },
+  {
+    "revision": "d2bb3c3c4210fcd14527acd266e0d39f",
+    "url": "/lenariem/movies-list-app_hooks/static/media/cat.d2bb3c3c.jpg"
+  },
+  {
+    "revision": "e3dd9c25ac5d0e36449404776e2f8905",
+    "url": "/lenariem/movies-list-app_hooks/static/media/cat2.e3dd9c25.jpg"
+  }
+]);
